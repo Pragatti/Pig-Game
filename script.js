c0bbbac4866ff3dfd8eedcd83b657ae6a2642699
pragatti harchand <pragatti4667@gmail.com>
2023-05-08 19:12:56 +0530

    active-player-functionality-done

--- script.js
+++ script.js
@@ -100,21 +100,41 @@
 
 // pig game 
 const score0El = document.querySelector('#score--0');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score1El = document.querySelector('#score--1');
-const diceEl =document.querySelector('.dice')
+const diceEl =document.querySelector('.dice');
 const  btnNew = document.querySelector('.btn-new')
 const  btnRoll = document.querySelector('.btn--roll')
 const  btnHold = document.querySelector('.btn-hold');
+let current0El = document.getElementById("current--0");
+let current1El = document.getElementById("current--1");
+const score =[0,0];
+let currentScore =0;
+let activePlayer = 0;
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
 btnRoll.addEventListener('click',function(){
-console.log("click on me")
     const dice = Math.trunc(Math.random()*6)+1;
     diceEl.classList.remove('hidden');
     diceEl.src =`dice-${dice}.png`;
-    console.log(diceEl)
+    if (dice !==1){
+      currentScore = currentScore+dice;
+    
+      document.getElementById(`current--${activePlayer}`).textContent=currentScore;
 
+  
+    }
+    else{
+        document.getElementById(`current--${activePlayer}`).textContent=0;
+        currentScore =0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+          player1El.classList.toggle('player--active');
+     
+    }
+     
 })
 
